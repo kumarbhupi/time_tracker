0134b2256cafdb30ad948cb611124638fd561d13
kumarbhupi <kumar.bhupi13@gmail.com>
2020-10-12 11:43:18 +0200

    Method for saving data in json formated file created in DataStorage

--- src/main/java/DataStorage.java
+++ src/main/java/DataStorage.java
@@ -1,13 +1,26 @@
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 import static java.lang.Thread.sleep;
 
+
 public class DataStorage {
-  JSONObject jsonObject;
+  public static final String JSON_SAVED_FILE_NAME = "json_data.txt";
+
+  public void saveData(Tracker toSave){
+
+    JSONArray trackerListJson = new JSONArray();
+    trackerListJson.put(toSave.getJSON());
+    try(FileWriter file = new FileWriter(JSON_SAVED_FILE_NAME)){
+      file.write(trackerListJson.toString());
+      file.flush();
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+  }
+
 
 
   public static void main(String[] args) throws InterruptedException {
@@ -31,16 +44,11 @@ public class DataStorage {
 
 
 
-    JSONArray trackerListJson = new JSONArray();
-    trackerListJson.put(tracker.getJSON());
 
 
-    try(FileWriter file = new FileWriter("test.json")){
-      file.write(trackerListJson.toString());
-      file.flush();
-    }catch (IOException e){
-      e.printStackTrace();
-    }
+
+
+
 
 
   }
